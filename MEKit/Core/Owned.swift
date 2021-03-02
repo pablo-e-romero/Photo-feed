2fee04ab95b13d340bb2991e77eec600cbbc3ebe
Pablo Romero <pablo.romero@glovoapp.com>
2021-03-02 17:37:57 +0100

    Refactor owned file

--- MEKit/Core/Owned.swift
+++ MEKit/Core/Owned.swift
@@ -7,16 +7,17 @@
 
 import Foundation
 
+
 struct OwnedKeys {
-  static var ownerKey: String = "OwnerKey"
+    static var ownerKey: String = "OwnerKey"
 }
 
 protocol Owned: AnyObject {
-  var owner: Any? {get set}
+    var owner: Any? { get set }
 }
 
 extension Owned {
-    
+
   var owner: Any? {
     get {
       return objc_getAssociatedObject(self, &OwnedKeys.ownerKey)
@@ -25,12 +26,14 @@ extension Owned {
     set {
       if let currentOwner = objc_getAssociatedObject(self, &OwnedKeys.ownerKey) {
         objc_setAssociatedObject(currentOwner, &OwnedKeys.ownerKey, nil, .OBJC_ASSOCIATION_ASSIGN)
+        objc_setAssociatedObject(self, &OwnedKeys.ownerKey, nil, .OBJC_ASSOCIATION_ASSIGN)
       }
 
       if let newValue = newValue {
         objc_setAssociatedObject(newValue, &OwnedKeys.ownerKey, self, .OBJC_ASSOCIATION_RETAIN)
+        objc_setAssociatedObject(self, &OwnedKeys.ownerKey, newValue, .OBJC_ASSOCIATION_ASSIGN)
       }
     }
   }
-    
+
 }
